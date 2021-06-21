6083468ac229dab12132e239a7ddac0ce60448c9
Agus Rahmat <agus.rahmat.18@gmail.com>
2021-06-21 15:47:18 +0700

    first commit

---
+++ app/Http/Controllers/SiswaController.php
@@ -0,0 +1,40 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use App\Siswa;
+
+class SiswaController extends Controller
+{
+    public function index(){
+		return Siswa::all();
+    }
+	
+	public function create(Request $request){
+		$siswa = new Siswa;
+		$siswa->nama = $request->nama;
+		$siswa->alamat = $request->alamat;
+		$siswa->save();
+		return "Data berhasil ditambahkan";
+    }
+	
+	public function read(Request $request, $id){
+		$siswa = Siswa::find($id);
+		return ($siswa);
+	}
+
+	public function update(Request $request, $id){
+		$nama = $request->nama;
+		$alamat = $request->alamat;
+		$siswa = Siswa::find($id);
+		$siswa->nama = $nama;
+		$siswa->alamat = $alamat;
+		$siswa->save();
+		return "Data berhasil diubah";
+    }
+	
+	public function delete($id){
+		$siswa = Siswa::find($id);
+		$siswa->delete();
+		return "Data berhasil dihapus";
+    }
+}
